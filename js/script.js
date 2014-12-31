95976d249593e74aa9fccb550b33661382f23637
Ankur Agarwal <Ankur.Agarwal@247-inc.com>
2014-12-31 10:48:33 +0530

    updated bug with IE10

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-if(document.attachEvent) {
-	document.attachEvent('onpaste', onPasteTriggered);
-}
-else if(document.addEventListener) {
+if(document.addEventListener) {
 	document.addEventListener('paste', onPasteTriggered, false);
 }
 
 /*Called whenever a paste event has occured*/
 function onPasteTriggered(e) {
-	var copiedData = e.clipboardData.items[0]; //Get the clipboard data
+	if(typeof e.clipboardData != 'undefined') {
+		var copiedData = e.clipboardData.items[0]; //Get the clipboard data
 
-	/*If the clipboard data is of type image, read the data*/
-	if(copiedData.type.indexOf("image") == 0) {
-		var imageFile = copiedData.getAsFile(); 
+		/*If the clipboard data is of type image, read the data*/
+		if(copiedData.type.indexOf("image") == 0) {
+			var imageFile = copiedData.getAsFile(); 
 
-		/*We will use HTML5 FileReader API to read the image file*/
-		var reader = new FileReader();
-		
-		reader.onload = function (evt) {
-			var result = evt.target.result; //base64 encoded image
+			/*We will use HTML5 FileReader API to read the image file*/
+			var reader = new FileReader();
+			
+			reader.onload = function (evt) {
+				var result = evt.target.result; //base64 encoded image
 
-			/*Create an image element and append it to the content editable div*/
-			var img = document.createElement("img");
-			img.src = result;
-			document.getElementById("editablediv").appendChild(img);
-        };
+				/*Create an image element and append it to the content editable div*/
+				var img = document.createElement("img");
+				img.src = result;
+				document.getElementById("editablediv").appendChild(img);
+		    };
 
-        /*Read the image file*/
-        reader.readAsDataURL(imageFile);
+		    /*Read the image file*/
+		    reader.readAsDataURL(imageFile);
+		}
 	}
 }
-
